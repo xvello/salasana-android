fae08d64aa8df2600c798a86018e601e4055a432
Xavier Vello <xavier.vello@gmail.com>
2019-05-05 21:31:06 +0200

    Port to api28

--- app/src/main/java/net/xvello/salasana/MainActivity.java
+++ app/src/main/java/net/xvello/salasana/MainActivity.java
@@ -1,49 +1,59 @@
 package net.xvello.salasana;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
+
+import androidx.core.content.ContextCompat;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Build;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
+import androidx.core.app.NotificationCompat;
 import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
+    private String mNotifChannel;
+    private NotificationManager mNotificationManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText domain = (EditText) findViewById(R.id.domain_edit);
-        EditText pass =  (EditText) findViewById(R.id.pass_edit);
-        TextView hint = (TextView) findViewById(R.id.hint_text);
+        EditText domain = findViewById(R.id.domain_edit);
+        EditText pass   = findViewById(R.id.pass_edit);
+        TextView hint   = findViewById(R.id.hint_text);
 
         domain.requestFocus();
 
+        mNotifChannel = getString(R.string.clipboard_chan_name);
+        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(mNotifChannel, mNotifChannel, NotificationManager.IMPORTANCE_LOW);
+            mNotificationManager.createNotificationChannel(channel);
+        }
+
         Intent intent = this.getIntent();
         if (intent != null) {
-            if (intent.getAction().equalsIgnoreCase(Intent.ACTION_SEND) && intent.hasExtra(Intent.EXTRA_TEXT)) {
+            if (Intent.ACTION_SEND.equalsIgnoreCase(intent.getAction()) && intent.hasExtra(Intent.EXTRA_TEXT)) {
                 try {
                     URL url = new URL(intent.getStringExtra(Intent.EXTRA_TEXT));
                     String host = url.getHost();
@@ -66,10 +76,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_about:
-                startActivity(new Intent(this, AboutActivity.class));
-                break;
+        if (item.getItemId() == R.id.action_about) {
+            startActivity(new Intent(this, AboutActivity.class));
         }
         return true;
     }
@@ -78,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
     private static String encodePassword(String password) throws NoSuchAlgorithmException, UnsupportedEncodingException {
         String result;
         MessageDigest md = MessageDigest.getInstance("SHA-1");
-        md.update(password.getBytes("UTF-8"), 0, password.length());
+        md.update(password.getBytes(StandardCharsets.UTF_8), 0, password.length());
         byte[] sha1hash = md.digest();
         result = Base64.encodeToString(sha1hash, Base64.DEFAULT);
         result = result.substring(0, 13)+"@1a";
@@ -86,8 +94,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void generatePassword(View view) throws UnsupportedEncodingException, NoSuchAlgorithmException {
-        EditText pass =  (EditText) findViewById(R.id.pass_edit);
-        EditText domain = (EditText) findViewById(R.id.domain_edit);
+        EditText pass = findViewById(R.id.pass_edit);
+        EditText domain = findViewById(R.id.domain_edit);
 
         ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
         ClipData clip = ClipData.newPlainText("Generated Password",
@@ -97,30 +105,28 @@ public class MainActivity extends AppCompatActivity {
         Intent serviceIntent = new Intent(this, ClipboardResetService.class);
         PendingIntent resetIntent = PendingIntent.getService(this.getApplicationContext(), 0, serviceIntent, 0);
 
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, mNotifChannel)
                 .setSmallIcon(R.drawable.notification_icon)
                 .setContentTitle(getString(R.string.notification_title))
                 .setContentText(getString(R.string.notification_content))
                 .setDeleteIntent(resetIntent)
                 .setColor(ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary));
 
-        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         mNotificationManager.notify(R.string.notification_id, mBuilder.build());
 
         this.finish();
     }
 
     public void resetForm(View view) {
-        EditText pass =  (EditText) findViewById(R.id.pass_edit);
+        EditText pass =  findViewById(R.id.pass_edit);
         pass.setText("");
-        EditText domain = (EditText) findViewById(R.id.domain_edit);
+        EditText domain = findViewById(R.id.domain_edit);
         domain.setText("");
 
         ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
         ClipData clip = ClipData.newPlainText("Generated Password", "");
         clipboard.setPrimaryClip(clip);
 
-        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         mNotificationManager.cancelAll();
     }
 }
